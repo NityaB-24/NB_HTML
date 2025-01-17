3b3c705c825c3b7d3f652db7ffffd7ea8e2dec0d
Nitya Balar <nitya.balar@anblicks.com>
2025-01-17 10:37:04 +0530

    added transitions

--- js/script.js
+++ js/script.js
@@ -1,20 +1,29 @@
 // Toggle mobile menu
-document.addEventListener('DOMContentLoaded', function () {
-  const toggleButton = document.querySelector('.navbar .mobile-menu-toggle');
-  const mobileMenu = document.querySelector('.navbar .mobile-menu-items');
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.querySelector(".navbar .mobile-menu-toggle");
+  const mobileMenu = document.querySelector(".navbar .mobile-menu-items");
 
-  toggleButton.addEventListener('click', function () {
-    mobileMenu.classList.toggle('active');
+  toggleButton.addEventListener("click", function () {
+    mobileMenu.classList.toggle("active");
   });
 });
 
 // Change navbar background on scroll
-window.addEventListener('scroll', function () {
-  const navbar = document.querySelector('.navbar');
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
 
   if (window.scrollY > 0) {
-    navbar.classList.add('navbar-scroll');
+    navbar.classList.add("navbar-scroll");
   } else {
-    navbar.classList.remove('navbar-scroll');
+    navbar.classList.remove("navbar-scroll");
   }
+});
+
+window.addEventListener("load", () => {
+  const image = document.querySelector(".hero img");
+  const h1section = document.querySelector(".hero h1");
+  const psection = document.querySelector(".hero p");
+  image.classList.add("loaded");
+  h1section.classList.add("loaded");
+  psection.classList.add("loaded");
 });
